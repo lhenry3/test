2789c7df34b0ca27b546de703fd34f7cc695db9c
lhenry3 <32119812+lhenry3@users.noreply.github.com>
2017-09-20 06:46:23 +0200

    change 1

---
+++ backup_delete.new.php
@@ -0,0 +1,53 @@
+<?php include 'session.php'; ?>
+<?php include 'function.php'; ?>
+<?php include 'ffam-function.php'; ?>
+<?php include 'decide_lang.php'; ?>
+
+<?php
+// Ajout v2.1
+if ($_SESSION['username'] == "ANONYMOUS") {
+  echo TXT_LOGGEDOUT;
+  exit();
+}
+// Fin ajout v2.1
+?>
+
+<?php
+	$rep = "backup";
+
+  echo "mlqsdkqmsl";
+
+	$sqladmin = "
+	  SELECT
+		paramvalue
+	  FROM
+		core_settings
+	  WHERE
+		paramname = 'INSTALLATION'";
+	$rowisntall = $PROD->GetRow($sqladmin);
+
+	if ($rowisntall['paramvalue'] == 'WINDOWS') {
+		$rep = $rep."\\";
+		$rmcmd = "del";
+
+		$sqlinstmysql = "
+		  SELECT
+			paramvalue
+		  FROM
+			core_settings
+		  WHERE
+			paramname = 'INSTREPMYSQL'
+			AND paramactive = 1";
+		$rowinstmysql = $PROD->GetRow($sqlinstmysql);
+		$repmysql = str_replace("\\", "\\\\", $rowinstmysql['paramvalue']);
+	} else {
+		$rep = $rep."/";
+		$rmcmd = "rm";
+		$repmysql = "/usr/sbin/";
+	}
+
+  system($rmcmd." ".$rep.PROD_DBNAME."*.sql");
+
+
+
+?>
